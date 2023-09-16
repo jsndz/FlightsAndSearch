8c9522eb6d43534891366baf65003febb572da9a
jsndz <jaisondz9360@gmail.com>
2023-09-16 20:43:23 +0530

    "dotenv"

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const {PORT} = require('./config/serverconfig')
 const setupAndstartserver = async () =>
 {
     const app = express();
-    
+     
     app.listen(3000, () =>
     {
         console.log(`server started at ${PORT}`);
