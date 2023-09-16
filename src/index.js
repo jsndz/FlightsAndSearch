ae123e13708881209966b5318d3129daf58fb63b
jsndz <jaisondz9360@gmail.com>
2023-09-16 21:03:18 +0530

    "bodyParser setup"

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverconfig')
 const setupAndstartserver = async () =>
 {
     const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}))
      
     app.listen(3000, () =>
     {
