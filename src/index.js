50d5108eda215f31a1c9397b5e2a7ca04b3b332d
jsndz <jaisondz9360@gmail.com>
2023-09-17 12:56:32 +0530

    "repo"

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {PORT} = require('./config/serverconfig')
+const {PORT} = require('./config/serverconfig');
+
 const setupAndstartserver = async () =>
 {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}))
      
-    app.listen(3000, () =>
+    app.listen(PORT, async () =>
     {
         console.log(`server started at ${PORT}`);
         
